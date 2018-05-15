5ba98a1b1643f5885d3904f8e21d04fea856bf04
DawidRos <dawid.rostk@gmail.com>
2018-05-15 15:36:56 +0200

    kompletny kalkulator 01

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cctype>
+#include <cmath>
 
 int kol_znak(char a);
 void add_to_exit(char * exit,int  & i_exit , char ch);
 void add_to_sign(char * sign, int & i_sig, char ch);
 void sign_to_exit(char *exit, int & i_exit, char *sign, int & i_sign);
+double licz(char * tab, int d1, int d2);
 
 int main()
 {
@@ -17,8 +19,9 @@ int main()
     int i_znak = 0;
     char z = 'k';
     while(z=='k'||z=='K') {
-        cout << "Podaj rownanie w standardowej notacji <bez spacji>,\nznaki operacji matematycznych to: ( ) ^ * / % + -\n";
-        cin.getline(tab, 100);
+        cout << "Podaj rownanie  w standardowej notacji <bez spacji>,\nznaki operacji matematycznych to: ( ) ^ * / % + -\n";
+        cin.getline(tab, 99);
+        i_znak=0;
         int i;
         for (i = 0; tab[i] != '\0'; i++) {
             switch (tab[i]) {
@@ -31,7 +34,7 @@ int main()
                 case '*':
                 case '/':
                 case '%':
-                    if (i_znak == 1 || kol_znak(tab[i]) > kol_znak(znak[i_znak - 1]))
+                    if (i_znak == 0 || kol_znak(tab[i]) > kol_znak(znak[i_znak - 1]))
                         add_to_sign(znak, i_znak, tab[i]);
                     else {
                         while (kol_znak(tab[i]) <= kol_znak(znak[i_znak - 1]))
@@ -55,11 +58,102 @@ int main()
         }
         while (i_znak != 0)
             sign_to_exit(odp, i_odp, znak, i_znak);
-        cout << "W odwroconej notacji polskiej to bedzie:\n";
-
-
+        cout << "W odwroconej notacji polskiej:\n";
         for (int j = 0; j < i_odp; j++)
             cout << odp[j];
+        double liczby[100] = { '\0' };
+        int i_liczby = 0;
+        char znaki[100] = { '\0' };
+        int i_znak = 0;
+        double wynik = 0;
+        int t1, t2;
+        t1 = 0;
+        t2 = 0;
+        for ( i = 0; odp[i] != '\0'; i++) {
+            double x1, x2;
+            switch (odp[i]) {
+                case '+':
+                    i_liczby--;
+                    x1 = liczby[i_liczby];
+                    liczby[i_liczby]=0;
+                    i_liczby--;
+                    x2 = liczby[i_liczby];
+                    liczby[i_liczby] = x1 + x2;
+                    i_liczby++;
+                    break;
+                case '-':
+                    i_liczby--;
+                    x1 = liczby[i_liczby];
+                    liczby[i_liczby]=0;
+                    i_liczby--;
+                    x2 = liczby[i_liczby];
+                    liczby[i_liczby] = x2 - x1;
+                    i_liczby++;
+                    break;
+                case '*':
+                    i_liczby--;
+                    x1 = liczby[i_liczby];
+                    liczby[i_liczby]=0;
+                    i_liczby--;
+                    x2 = liczby[i_liczby];
+                    liczby[i_liczby] = x2 * x1;
+                    i_liczby++;
+                    break;
+                case '/':
+                    i_liczby--;
+                    x1 = liczby[i_liczby];
+                    liczby[i_liczby]=0;
+                    i_liczby--;
+                    x2 = liczby[i_liczby];
+                    liczby[i_liczby] = x2 / x1;
+                    i_liczby++;
+                    break;
+                case '%':
+                    i_liczby--;
+                    x1 = liczby[i_liczby];
+                    liczby[i_liczby]=0;
+                    i_liczby--;
+                    x2 = liczby[i_liczby];
+                    liczby[i_liczby] = (int)x2 % (int)x1;
+                    i_liczby++;
+                    break;
+                case '^':
+                    i_liczby--;
+                    x1 = liczby[i_liczby];
+                    liczby[i_liczby]=0;
+                    i_liczby--;
+                    x2 = liczby[i_liczby];
+                    liczby[i_liczby] = pow(x2, x1);
+                    i_liczby++;
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    t1++;
+                    break;
+                case '.':
+                case ',':
+                    t2 = 1;
+                    liczby[i_liczby] = licz(odp + i, t1,0);
+                    break;
+                case ' ':
+                    if (isdigit(odp[i - 1])|| odp[i - 1]=='.'|| odp[i - 1] == ',') {
+                        liczby[i_liczby] +=licz(odp+i, t1,t2);
+                        i_liczby++;
+                        t1 = 0;
+                        t2 = 0;
+                    }
+                    break;
+            }
+        }
+        cout << "\nWynik: " << liczby[i_liczby-1] << endl;
         cout <<"\npodaj <k> aby zadac kolejne rownanie: ";
         cin >> z;
         for (int i=0;i<100;i++)
@@ -80,7 +174,7 @@ int kol_znak(char a)
         return 3;
     else if (a == '*' || a == '/'||a=='%')
         return 2;
-    else if (a == '+' || a == '-')
+    else if (a == '+' || a == '-'||a==')')
         return 1;
     else
         return 0;
@@ -108,5 +202,34 @@ void sign_to_exit(char *exit, int & i_exit, char *sign, int & i_sign)
     *(sign + i_sign) = '\0';
     *(exit +i_exit+1)=' ';
     i_exit+=2;
+}
+
+double licz(char * tab, int d1,int d2)
+{
+    double m = 0;
+    double y;
+    int i;
+    if (d2 == 0)
+    {
+        for (int i = 1; i <= d1; i++)
+        {
+            y = *(tab - i) - 48;
+            m += y * pow(10, i - 1);
+        }
+    }
+
+    else if (d2 == 1)
+    {
+        int k = 1;
+        char * pc;
+        for (i = 1; *(tab - i) != '.' && *(tab - i) != ','; i++) {}
+        pc = (tab - i);
+        for ( int j = 1; j<i ; j++)
+        {
+            y = *(pc+j) - 48;
+            m += y * pow(10, -j);
+        }
+    }
+    return m;
 }
 
