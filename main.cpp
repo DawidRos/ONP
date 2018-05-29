8138863d1594dde567adb1b4ff1c4f9e8371d988
solanik <solanik@wp.pl>
2018-05-29 20:27:13 +0200

    My comments

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include <cctype>
 #include <cmath>
 
+// TODO: Translate everything into english (kol_znak, licz...)
+// TODO: I'd go with defining functions in place, instead of making declarations here and definitions lower
+//       That's rather not a popular approach
+// TODO: Be consistent with separators - either "char* name" or "char * name" or "char *name". (I use first one, but it's not written in stone)
+//       The same with & and with ,
+// TODO: It'd be great to see a class, that has a field char* with the equation. And on it you can use those different actions
+//       That should help with being sure that we don't move past the last element of table (especially when you do += 2)
+// TODO: Think about using std::stringstream isntead of char*. It should be more usefull, as it can use operator<< that can directly
+//       translate input into multiple different types like int, double and so on
 int kol_znak(char a);
 void add_to_exit(char * exit,int  & i_exit , char ch);
 void add_to_sign(char * sign, int & i_sig, char ch);
@@ -23,6 +32,7 @@ int main()
         cin.getline(tab, 99);
         i_znak=0;
         int i;
+        // TODO: This for should be in a single function
         for (i = 0; tab[i] != '\0'; i++) {
             switch (tab[i]) {
                 case '(':
@@ -63,6 +73,7 @@ int main()
             cout << odp[j];
         double liczby[100] = { '\0' };
         int i_liczby = 0;
+        // TODO: What's the difference between znak[100] and znaki[100] ? I think you actually don't use it
         char znaki[100] = { '\0' };
         int i_znak = 0;
         double wynik = 0;
@@ -72,6 +83,8 @@ int main()
         for ( i = 0; odp[i] != '\0'; i++) {
             double x1, x2;
             switch (odp[i]) {
+                // TODO: All those cases have the same implementations, that differs only with operators on used on numbers.
+                //       It should be possible to make a function that shares the implementation that can be shared.
                 case '+':
                     i_liczby--;
                     x1 = liczby[i_liczby];
@@ -180,7 +193,11 @@ int kol_znak(char a)
         return 0;
 }
 
-
+// TODO: Accessing element with operators [] is more readable
+// TODO: i_exit+=2 sounds like a side effect to me. Your function says it's adding to exit, and that's good one responsibility for it.
+//       One of the rules of programming is https://en.wikipedia.org/wiki/Single_responsibility_principle, and having methods add
+//       element to table and change some index that has been passed to them sounds like a violation of that principal
+//       Probably if it was a class and this method would change some field "current_index" it would be more OK
 void add_to_exit(char * exit, int  & i_exit, char ch)
 {
     *(exit+i_exit) = ch;
@@ -204,6 +221,10 @@ void sign_to_exit(char *exit, int & i_exit, char *sign, int & i_sign)
     i_exit+=2;
 }
 
+// TODO: Names should be meaningful. What is d1 and d2? The same with m and y.
+// TODO: You don't need to declare i variable at start.
+// TODO: I really don't like that you iterate through the table in the opposite direction - that the tab is pointing to the end of table.
+//       This seems very error prone and unnatural. I'd advice always using the pointer to the beginning of the table.
 double licz(char * tab, int d1,int d2)
 {
     double m = 0;
@@ -220,7 +241,7 @@ double licz(char * tab, int d1,int d2)
 
     else if (d2 == 1)
     {
-        int k = 1;
+        int k = 1; // TODO: I cannot see it being used
         char * pc;
         for (i = 1; *(tab - i) != '.' && *(tab - i) != ','; i++) {}
         pc = (tab - i);
